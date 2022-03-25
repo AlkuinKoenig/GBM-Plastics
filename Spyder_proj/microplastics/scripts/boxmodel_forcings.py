47081de3205a81d8d491c01f907866eefca55227
AlkuinKoenig <alkuin.koenig@gmx.de>
2022-03-25 20:39:08 +0100

    'added_flux_example;

--- Spyder_proj/microplastics/scripts/boxmodel_forcings.py
+++ Spyder_proj/microplastics/scripts/boxmodel_forcings.py
@@ -5,13 +5,19 @@ class boxmodel_forcings():
     
     def __init__(self, scenario = ("base")):
         self.scenario = scenario
+        if not self.scenario in ["base","fullstop"]:
+            raise Exception("Error. Undefined scenario. Be sure to initiate your 'forcings' class with a valid scenario! Typo?")
+        
+    # if not (self.scenario[0] in ("base","fullstop")):
+    #         return np.nan
         
     
-    def get_P_prod(self,time):
+    def get_P_prod(self,time):        
         P_prod = np.where(time < 1950, 0, (0.1045809 * time ** 2 - 409.084300 * time + 400055.2))
         P_prod = P_prod * 8300/8007 # tweaking the function so that total produced form 1950 to 2015 (including) is 8300.
         if (self.scenario[0] == "fullstop"):
             P_prod = np.where(time >= self.scenario[1], 0, P_prod)
+            
         
         return (P_prod)
 
@@ -37,3 +43,8 @@ class boxmodel_forcings():
         f_rec = np.where(time < 1989, 0, 0.00712723 * time - 14.1653)  # before 1989=0; since 1989 use equation
         return (f_rec)
 
+# FRCS = boxmodel_forcings(("base"))
+
+# print(FRCS.get_P_prod(2000))
+# # print(FRCS.get_f_incin(1981))
+# print(1 - FRCS.get_f_incin(1980) - FRCS.get_f_rec(1980) )
---
+++ Spyder_proj/microplastics/scripts/plot_model_results_V3.py
@@ -0,0 +1,97 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#################
+
+fpath = "../../../output/"
+#insert the output file name here
+fname = "OUTP_V3_20220325_2018_TSPAN_1950-2100_SCEN_('fullstop', 2025)_INP_PARS_BASE_V2_20220322_1739.csv"
+
+#################
+
+mout = pd.read_csv(fpath + fname, skiprows = 60)
+
+print(f"Available data columns: {mout.columns}")
+
+mout["control"]  = mout[["P_use","P_disc","MP_disc",
+               "sMP_disc","P_SurfOce","MP_SurfOce","sMP_SurfOce",
+               "MP_DeepOce","sMP_DeepOce","sMP_atm","sMP_soil","P_beach",
+               "MP_beach","sMP_beach","P_ShelfSed","MP_ShelfSed", "sMP_ShelfSed",
+               "MP_DeepSed","sMP_DeepSed"]].sum(axis=1)
+
+
+plt.plot(mout["Year"], mout["P_use"], "-", label = "P use")
+plt.plot(mout["Year"], mout["P_disc"], "-", label = "P disc")
+plt.plot(mout["Year"], mout["MP_disc"], "-", label = "MP disc")
+plt.plot(mout["Year"], mout["sMP_disc"], "-", label = "sMP disc")
+plt.plot(mout["Year"], mout["sMP_soil"], "-", label = "sMP soil")
+plt.xlabel("year")
+plt.ylabel("mass [tonnes]")
+plt.legend()
+plt.show()
+
+
+
+plt.plot(mout["Year"], mout["P_SurfOce"],"-", label = "P surface ocean")
+plt.plot(mout["Year"], mout["MP_SurfOce"],"-", label = "MP surface ocean")
+plt.plot(mout["Year"], mout["sMP_SurfOce"],"-", label = "sMP surface ocean")
+plt.plot(mout["Year"], mout["MP_DeepOce"],"-", label = "MP deep ocean")
+plt.plot(mout["Year"], mout["sMP_DeepOce"],"-", label = "sMP deep ocean")
+plt.xlabel("year")
+plt.ylabel("mass [tonnes]")
+plt.legend()
+plt.show()
+
+
+plt.plot(mout["Year"], mout["P_beach"],"-", label = "P beach")
+plt.plot(mout["Year"], mout["MP_beach"],"-", label = "MP beach")
+plt.plot(mout["Year"], mout["sMP_beach"],"-", label = "sMP beach")
+plt.yscale("log")
+plt.xlabel("year")
+plt.ylabel("mass [tonnes]")
+plt.legend()
+plt.show()
+
+
+plt.plot(mout["Year"], mout["sMP_atm"], "-", label = "sMP_atm")
+plt.plot(mout["Year"], mout["sMP_soil"],"-", label = "sMP_soil")
+#plt.yscale("log")
+plt.xlabel("year")
+plt.ylabel("mass [tonnes]")
+plt.legend()
+plt.show()
+
+
+plt.plot(mout["Year"], mout["sMP_DeepOce"] , "-", label = "sMP_DeepOce ")
+plt.plot(mout["Year"], mout["MP_DeepOce"] , "-", label = "MP_DeepOce ")
+#plt.yscale("log")
+plt.xlabel("year")
+plt.ylabel("mass [tonnes]")
+plt.legend()
+plt.show()
+
+plt.plot(mout["Year"], mout["F_atm_to_soil"] , "-", label = "total F_atm_to_soil")
+#plt.plot(mout["Year"], mout["MP_DeepOce"] , "-", label = "MP_DeepOce ")
+#plt.yscale("log")
+plt.xlabel("year")
+plt.ylabel("mass [tonnes]")
+plt.legend()
+plt.show()
+
+
+
+plt.plot(mout["Year"],mout["control"], "-",label = "all compartments")
+#plt.axvline(x=2015,color="red")
+plt.xlabel("year")
+plt.ylabel("mass [tonnes]")
+plt.legend()
+plt.show()
+
+
+print("\nup to this point:\n")
+print(f"All plastics ever produced: {np.array(mout['P_prod_tot'])[-1]}")
+print(f"\nAll plastics ever incinerated: {np.array(mout['P_inc_tot'])[-1]}")
+print(f"\ncontrol at last year of computation: {np.array(mout['control'])[-1]}")
+print(f"\nControl + all plastics ever incinerated (this should sum to \"ever produced\"): {np.array(mout['control'])[-1]+np.array(mout['P_inc_tot'])[-1]}")
+
